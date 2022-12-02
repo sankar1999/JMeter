8d7c557e8661ab38b91e535f97e371519be4fe3f
sankar1999 <sankarsathya1973@gmail.com>
2022-12-03 01:20:50 +0530

    JMeter

---
+++ jmeter/src/test/java/com/jmeter/JmeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
