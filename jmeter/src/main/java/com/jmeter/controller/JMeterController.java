cd7bb611f9891abc2b6562678347534e7363d515
sankar1999 <sankarsathya1973@gmail.com>
2022-12-03 03:57:25 +0530

    JMeter

--- jmeter/src/main/java/com/jmeter/controller/JMeterController.java
+++ jmeter/src/main/java/com/jmeter/controller/JMeterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("jmeter")
+@RequestMapping("/jmeter")
 public class JMeterController {
 
     @Autowired
